0da5a9d1f9be3f10ed05b4611c28314e2da84a53
Ben Argo <ben@benargo.com>
2012-04-05 05:05:10 +0100

    Still more bugs

--- update.php
+++ update.php
@@ -7,9 +7,9 @@ $xml = simplexml_load_file("availability.xml") or die("Unable to load XML");
 
 $select = $xml->xpath("//day[@date='". $day ."']/member[@id='". $member ."']");
 
-var_dump($select);
-
 $select[0]->attributes()->free = $newstatus;
+
+var_dump($xml->asXML())
 
 file_put_contents("availability.xml", $xml->asXML());
 ?>
